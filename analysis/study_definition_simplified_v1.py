d0759d05748e1d6eeb1db03e996c8d2bb16d053f
Becky Smith <becky.smith@thedatalab.org>
2021-07-08 19:04:08 +0100

    Simplified v1 and v2 study definitions

---
+++ analysis/study_definition_simplified_v1.py
@@ -0,0 +1,103 @@
+from cohortextractor import (
+    StudyDefinition,
+    Measure,
+    patients,
+    codelist,
+    combine_codelists,
+    codelist_from_csv,
+)
+
+from codelists import long_covid_diagnostic_codes, covid_primary_care_code, covid_codes
+from common_variables import demographic_variables
+
+pandemic_start = "2020-02-01"
+
+
+def generate_code_variables(codelist):
+    return {
+        f"snomed_{code}": (
+            patients.with_these_clinical_events(
+                codelist([code], system="snomed"),
+                on_or_after=pandemic_start,
+                returning="number_of_matches_in_period",
+                include_date_of_match=True,
+                date_format="YYYY-MM-DD",
+                return_expectations={
+                    "incidence": 0.1,
+                    "int": {"distribution": "normal", "mean": 3, "stddev": 1},
+                },
+            )
+        ) for code in codelist
+    }
+
+
+demographics = {k: v for k, v in demographic_variables.items() if k in ["age_group", "sex"]}
+
+study = StudyDefinition(
+    default_expectations={
+        "date": {"earliest": "index_date", "latest": "today"},
+        "rate": "uniform",
+        "incidence": 0.05,
+        "int": {"distribution": "normal", "mean": 25, "stddev": 5},
+        "float": {"distribution": "normal", "mean": 25, "stddev": 5},
+    },
+    index_date="2020-11-01",
+    population=patients.satisfying(
+        "registered AND (sex = 'M' OR sex = 'F')",
+        registered=patients.registered_as_of("index_date"),
+    ),
+    # COVID infection
+    sgss_positive=patients.with_test_result_in_sgss(
+        pathogen="SARS-CoV-2",
+        test_result="positive",
+        returning="date",
+        date_format="YYYY-MM-DD",
+        find_first_match_in_period=True,
+        return_expectations={"incidence": 0.1, "date": {"earliest": "index_date"}},
+    ),
+    primary_care_covid=patients.with_these_clinical_events(
+        covid_primary_care_code,
+        returning="date",
+        date_format="YYYY-MM-DD",
+        find_first_match_in_period=True,
+        return_expectations={"incidence": 0.1, "date": {"earliest": "index_date"}},
+    ),
+    hospital_covid=patients.admitted_to_hospital(
+        with_these_diagnoses=covid_codes,
+        returning="date_admitted",
+        date_format="YYYY-MM-DD",
+        find_first_match_in_period=True,
+        return_expectations={"incidence": 0.1, "date": {"earliest": "index_date"}},
+    ),
+    # Outcome
+    long_covid=patients.with_these_clinical_events(
+        long_covid_diagnostic_codes,
+        return_expectations={"incidence": 0.05},
+    ),
+    first_long_covid_date=patients.with_these_clinical_events(
+        long_covid_diagnostic_codes,
+        returning="date",
+        date_format="YYYY-MM-DD",
+        find_first_match_in_period=True,
+        return_expectations={"incidence": 0.1, "date": {"earliest": "index_date"}},
+    ),
+    **generate_code_variables(long_covid_diagnostic_codes),
+    first_long_covid_code=patients.with_these_clinical_events(
+        long_covid_diagnostic_codes,
+        returning="code",
+        find_first_match_in_period=True,
+        return_expectations={
+            "incidence": 0.05,
+            "category": {
+                "ratios": {
+                    "1325161000000102": 0.2,
+                    "1325181000000106": 0.2,
+                    "1325021000000106": 0.3,
+                    "1325051000000101": 0.2,
+                    "1325061000000103": 0.1,
+                }
+            },
+        },
+    ),
+    **demographics,
+)
