e82e27b8d06d3b71334fe73273bae807c31b48d4
Becky Smith <becky.smith@thedatalab.org>
2021-08-03 12:50:44 +0100

    Update codelists and project.yaml for v2

--- analysis/codelists_v2.py
+++ analysis/codelists_v2.py
@@ -1,6 +1,53 @@
-from cohortextractor import codelist_from_csv
+from cohortextractor import codelist, codelist_from_csv, combine_codelists
 
-
+covid_codes = codelist_from_csv(
+    "codelists/opensafely-covid-identification.csv",
+    system="icd10",
+    column="icd10_code",
+)
+covid_primary_care_positive_test = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-positive-test.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+covid_primary_care_code = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+covid_primary_care_sequalae = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-sequelae.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+any_primary_care_code = combine_codelists(
+    covid_primary_care_code,
+    covid_primary_care_positive_test,
+    covid_primary_care_sequalae,
+)
+long_covid_diagnostic_codes = codelist_from_csv(
+    "codelists/opensafely-nice-managing-the-long-term-effects-of-covid-19.csv",
+    system="snomed",
+    column="code",
+)
+long_covid_referral_codes = codelist_from_csv(
+    "codelists/opensafely-referral-and-signposting-for-long-covid.csv",
+    system="snomed",
+    column="code",
+)
+long_covid_assessment_codes = codelist_from_csv(
+    "codelists/opensafely-assessment-instruments-and-outcome-measures-for-long-covid.csv",
+    system="snomed",
+    column="code",
+)
+any_long_covid_code = combine_codelists(
+    long_covid_diagnostic_codes, long_covid_referral_codes, long_covid_assessment_codes
+)
+post_viral_fatigue_codes = codelist_from_csv(
+    "codelists/user-alex-walker-post-viral-syndrome.csv",
+    system="snomed",
+    column="code",
+)
 ethnicity_codes = codelist_from_csv(
     "codelists/opensafely-ethnicity.csv",
     system="ctv3",
